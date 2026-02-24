e44058119e46e9870e0dc3150855cd41543fbaea
hadibavili <bavilihadi@gmail.com>
2026-02-24 17:01:57 +0100

    Initial Commit

---
+++ Mind Chat/Mind_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mind_ChatApp.swift
+//  Mind Chat
+//
+//  Created by Hadi Bavili on 24/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct Mind_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
